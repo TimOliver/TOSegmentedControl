a8ba82e5505e864ac90293a9324199c444735dff
Tim Oliver <me@timoliver.com.au>
2019-09-10 22:32:19 -0400

    Improved the sample app

--- TOSegmentedControlExample/ViewController.m
+++ TOSegmentedControlExample/ViewController.m
@@ -11,7 +11,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *classicSegmentedControl;
 @property (weak, nonatomic) IBOutlet TOSegmentedControl *segmentedControl;
+
+@property (weak, nonatomic) IBOutlet UILabel *classicSegmentedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *segmentedLabel;
 
 @end
 
@@ -20,7 +24,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.classicSegmentedLabel.alpha = 0.0f;
+    self.segmentedLabel.alpha = 0.0f;
+    
+    __weak typeof(self) weakSelf = self;
     self.segmentedControl.items = @[@"First", @"Second", @"Third"];
+    self.segmentedControl.segmentTappedHandler = ^(NSInteger index, BOOL reversed) {
+        NSString *title = [self nameForIndex:index];
+        [weakSelf animateLabel:weakSelf.segmentedLabel title:title];
+    };
+}
+
+- (IBAction)segmentedControlUpdated:(id)sender
+{
+    NSString *title = [self nameForIndex:self.classicSegmentedControl.selectedSegmentIndex];
+    [self animateLabel:self.classicSegmentedLabel title:title];
+}
+
+- (void)animateLabel:(UILabel *)label title:(NSString *)title
+{
+    label.text = title;
+    label.alpha = 1.0f;
+    label.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1f, 1.1f);
+    
+    [UIView animateWithDuration:1.0f
+                          delay:0.0f
+         usingSpringWithDamping:0.3f
+          initialSpringVelocity:50.0f
+                        options:UIViewAnimationOptionBeginFromCurrentState
+                     animations:^{ label.transform = CGAffineTransformIdentity; }
+                     completion:^(BOOL finished)
+    {
+        [UIView animateWithDuration:0.4f animations:^{
+            label.alpha = 0.0f;
+        }];
+    }];
+}
+
+- (NSString *)nameForIndex:(NSInteger)index
+{
+    switch (index) {
+        case 0: return @"First Selected";
+        case 1: return @"Second Selected";
+        case 2: return @"Third Selected";
+    }
+    
+    return nil;
 }
 
 @end
