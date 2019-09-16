4886183b18756cdfc0f0892d56de76cae8fd5dc7
Tim Oliver <me@timoliver.com.au>
2019-09-16 12:03:58 +0900

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,21 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "TOSegmentedControl",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TOSegmentedControl",
             targets: ["TOSegmentedControl"]),
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TOSegmentedControl",
-            dependencies: []),
+            dependencies: [],
+            path: "TOSegmentedControl",
+            publicHeadersPath: "TOSegmentedControl"),
         .testTarget(
             name: "TOSegmentedControlTests",
-            dependencies: ["TOSegmentedControl"]),
+            dependencies: ["TOSegmentedControl"],
+            path: "TOSegmentedControlTests"),
     ]
 )
