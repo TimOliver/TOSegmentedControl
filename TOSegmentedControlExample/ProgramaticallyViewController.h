2bb045af5a0b34e4c01a1e1ff1cd59e184fe886f
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-05-17 23:02:55 +0100

    Add Programatically views

---
+++ TOSegmentedControlExample/ProgramaticallyViewController.h
@@ -0,0 +1,14 @@
+//
+//  ProgramaticallyViewController.h
+//  TOSegmentedControlExample
+//
+//  Created by Pedro Paulo de Amorim on 17/05/2020.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgramaticallyViewController : UIViewController
+
+
+@end
---
+++ TOSegmentedControlExample/ProgramaticallyViewController.m
@@ -0,0 +1,82 @@
+//
+//  ProgramaticallyViewController.m
+//  TOSegmentedControlExample
+//
+//  Created by Pedro Paulo de Amorim on 17/05/2020.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+#import "ProgramaticallyViewController.h"
+#import "TOSegmentedControl.h"
+
+@interface ProgramaticallyViewController ()
+
+@property (nonatomic, strong) TOSegmentedControl *thirdSegmentedControl;
+@property (nonatomic, assign) BOOL didSetupConstraints;
+
+@end
+
+@implementation ProgramaticallyViewController
+
+- (void)loadView {
+  [super loadView];
+
+  self.view.backgroundColor = UIColor.whiteColor;
+  [self setupThirdSegmentedControl];
+  [self.view setNeedsUpdateConstraints];
+
+}
+
+- (void)updateViewConstraints {
+
+    if (!self.didSetupConstraints) {
+
+        self.didSetupConstraints = YES;
+
+        NSLayoutConstraint *leadingConstraint = [NSLayoutConstraint
+                                                 constraintWithItem:self.thirdSegmentedControl
+                                                  attribute:NSLayoutAttributeLeading
+                                                  relatedBy:NSLayoutRelationEqual
+                                                  toItem:self.view
+                                                  attribute:NSLayoutAttributeLeading
+                                                  multiplier:1.0f
+                                                  constant:0.f];
+        [self.view addConstraint:leadingConstraint];
+
+
+        NSLayoutConstraint *trailingConstraint = [NSLayoutConstraint
+                                                   constraintWithItem:self.thirdSegmentedControl
+                                                   attribute:NSLayoutAttributeTrailing
+                                                   relatedBy:NSLayoutRelationEqual
+                                                   toItem:self.view
+                                                   attribute:NSLayoutAttributeTrailing
+                                                   multiplier:1.0
+                                                   constant:0];
+        [self.view addConstraint:trailingConstraint];
+
+        NSLayoutConstraint *con4 = [NSLayoutConstraint
+                                    constraintWithItem:self.thirdSegmentedControl
+                                    attribute:NSLayoutAttributeHeight
+                                    relatedBy:0
+                                    toItem:nil
+                                    attribute:NSLayoutAttributeNotAnAttribute
+                                    multiplier:1
+                                    constant:50];
+        [self.view addConstraint:con4];
+
+    }
+
+    [super updateViewConstraints];
+}
+
+- (void)setupThirdSegmentedControl {
+    self.thirdSegmentedControl = [TOSegmentedControl new];
+    self.thirdSegmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [self.view addSubview:self.thirdSegmentedControl];
+    self.thirdSegmentedControl.items = @[@"First", @"Second"];
+    self.thirdSegmentedControl.itemColor = UIColor.blackColor;
+    self.thirdSegmentedControl.backgroundColor = UIColor.whiteColor;
+}
+
+@end
